8d1b527118d3e6b2023159bb24e33d7eae75568a
admin admin <evaladmin@example.com>
2023-11-28 15:22:07 +0800

    "User admin wrote content /scripts/pages/tenant-listing.groovy"

--- scripts/pages/tenant-listing.groovy
+++ scripts/pages/tenant-listing.groovy
@@ -24,7 +24,7 @@ templateModel.dummyText = dummyText
 def segment = ProfileUtils.getSegment(profile, siteItemService)
 def category = contentModel.category_s
 def maxArticles = contentModel.max_articles_i
-/*def searchHelper = new SearchHelper(searchClient, urlTransformationService)
+def searchHelper = new SearchHelper(searchClient, urlTransformationService)
 def articles = searchHelper.searchArticles(false, category, segment, 0, maxArticles)
 
-templateModel.articles = articles*/
+templateModel.articles = articles
