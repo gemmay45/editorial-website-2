f923a0fc0bf0b8a1df23fb3eea1665dd0bc8d9ce
admin admin <evaladmin@example.com>
2023-11-28 17:04:38 +0800

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/TenantSearchHelper.groovy"

--- scripts/classes/org/craftercms/sites/editorial/TenantSearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/TenantSearchHelper.groovy
@@ -194,7 +194,7 @@ class TenantSearchHelper {
             article.id = doc.objectId
             article.objectId = doc.objectId
             article.path = doc.localId
-            article.title = doc.title_t
+            article.name = doc.name_s
             article.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
 
         if (hit.highlight()) {
