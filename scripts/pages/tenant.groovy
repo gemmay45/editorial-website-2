e02df1a06ebec8452cd2f5635eb1da3c12f556e2
admin admin <evaladmin@example.com>
2023-11-29 12:28:26 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -5,4 +5,16 @@ recTenants << recTenant
 recTenant.title = "bbb"
 recTenants << recTenant
 
-templateModel.recTenants = recTenants
+
+    def dirs = contentModel.recommended_o
+    dirs.each { dir ->
+            def dirName = dir.getStoreName()
+            def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
+            if (dirItem != null) {
+                def dirDisplayName = dirItem.queryValue('internal-name')
+                   topNavItems.put(dirName, dirDisplayName)
+            }
+   }
+
+
+templateModel.recTenants = topNavItems
