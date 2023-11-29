8aac3cf0e052cebfacc4887dc0872fe291ba15aa
admin admin <evaladmin@example.com>
2023-11-30 07:48:43 +0800

    "User admin wrote content /scripts/pages/category-landing.groovy"

--- scripts/pages/category-landing.groovy
+++ scripts/pages/category-landing.groovy
@@ -24,6 +24,6 @@ def searchHelper = new SearchHelper(searchClient, urlTransformationService)
 def articles = searchHelper.searchArticles(false, category, segment, 0, maxArticles)
 
 
-println category
+log.info category
 
 templateModel.articles = articles
