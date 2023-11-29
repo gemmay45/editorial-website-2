901ed3958a1129d73dadf59900a5d7309f44e9ec
admin admin <evaladmin@example.com>
2023-11-29 21:29:21 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,10 +1,11 @@
 
 def recTenants = []
+def relatedMags = []
 def recs = contentModel.recommended_o.item
+def rels = contentModel.relatedMAGArticle_o.item
 recs.each { rec ->
     def recTenant = [:]
     
-    def dirName = rec.key.text
     def dirItem = siteItemService.getSiteItem(rec.key.text)
     
     recTenant.id = dirItem.objectId
@@ -18,6 +19,22 @@ recs.each { rec ->
     recTenant.images = dirItem.images_o
 
     recTenants << recTenant
+}
+
+rels.each { rel ->
+    def relatedMag = [:]
+    
+    def dirItem = siteItemService.getSiteItem(rel.key.text)
+    
+    recTenant.id = dirItem.objectId
+    recTenant.objectId = dirItem.objectId
+    recTenant.path = dirItem.localId
+    recTenant.storeUrl = dirItem.storeUrl
+    recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", recTenant.storeUrl)
+
+    recTenant.title = dirItem.queryValue('title_s')
+
+    relatedMags << relatedMag
 }
 
 
