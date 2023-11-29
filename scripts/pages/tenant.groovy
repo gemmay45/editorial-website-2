eb7126340030b434ed3b0965440280b37ee4e9ff
admin admin <evaladmin@example.com>
2023-11-29 21:19:51 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,35 +1,25 @@
 
-    def recTenants = []
-    def topNavItems = [:]
-    def dirs = contentModel.recommended_o.item
-    dirs.each { dir ->
-            def recTenant = [:]
-            
-            def dirName = dir.key.text
-            def dirItem = siteItemService.getSiteItem(dir.key.text)
-            
-            recTenant.id = dirItem.objectId
-            recTenant.objectId = dirItem.objectId
-            recTenant.path = dirItem.localId
-            recTenant.storeUrl = dirItem.storeUrl
-            recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", recTenant.storeUrl)
+def recTenants = []
+def topNavItems = [:]
+def dirs = contentModel.recommended_o.item
+dirs.each { dir ->
+    def recTenant = [:]
+    
+    def dirName = dir.key.text
+    def dirItem = siteItemService.getSiteItem(dir.key.text)
+    
+    recTenant.id = dirItem.objectId
+    recTenant.objectId = dirItem.objectId
+    recTenant.path = dirItem.localId
+    recTenant.storeUrl = dirItem.storeUrl
+    recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", recTenant.storeUrl)
 
-            recTenant.title = dirItem.queryValue('name_s')
-            recTenant.tagline = dirItem.queryValue('tagline_s')
-            recTenant.images = dirItem.images_o
+    recTenant.title = dirItem.queryValue('name_s')
+    recTenant.tagline = dirItem.queryValue('tagline_s')
+    recTenant.images = dirItem.images_o
 
-            recTenants << recTenant
-            
-                        /*
-                        def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
-            
-
-            if (dirItem != null) {
-                def dirDisplayName = dirItem.queryValue('internal-name')
-                   topNavItems.put(dirName, dirDisplayName)
-            }
-            */
-   }
+    recTenants << recTenant
+}
 
 
 templateModel.recTenants = recTenants
