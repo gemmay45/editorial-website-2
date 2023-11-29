1d1bc91ff4bbe208c8b9f932f78439084bbdd8fe
admin admin <evaladmin@example.com>
2023-11-29 16:18:07 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,38 +1,4 @@
 
-    def recTenants = []
-    def topNavItems = [:]
-    def dirs = contentModel.recommended_o.item
-    dirs.each { dir ->
-            def recTenant = [:]
-            
-            def dirName = dir.key.text
-            def dirItem = siteItemService.getSiteItem(dir.key.text)
-            
-            recTenant.id = dirItem.objectId
-            recTenant.objectId = dirItem.objectId
-            recTenant.path = dirItem.localId
-            recTenant.storeUrl = dirItem.storeUrl
-            recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", recTenant.storeUrl)
-
-            recTenant.dirItem = dirItem
-            recTenant.title = dirItem.queryValue('name_s')
-            recTenant.key = dir.key
-            recTenants << recTenant
-            
-                        /*
-                        def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
-            
-
-            if (dirItem != null) {
-                def dirDisplayName = dirItem.queryValue('internal-name')
-                   topNavItems.put(dirName, dirDisplayName)
-            }
-            */
-   }
-
-
-templateModel.recTenants = recTenants
-
 import org.craftercms.sites.editorial.TenantSearchHelper
 import org.craftercms.sites.editorial.ProfileUtils
 
