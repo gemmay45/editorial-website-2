427f438a90c3e2a2093d487ee1f1da3a497d30af
admin admin <evaladmin@example.com>
2023-11-29 14:52:42 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -5,14 +5,23 @@
     dirs.each { dir ->
             def recTenant = [:]
 
+            recTenant.id = dir.objectId
+            recTenant.objectId = dir.objectId
+            recTenant.path = dir.localId
+            recTenant.storeUrl = dir.localId
+            recTenant.title = dir.name_s
+            recTenant.summary = dir.summary_t
+            recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", dir.localId)
+        
+        
             def dirName = dir.key.text
             def dirItem = siteItemService.getSiteItem(dir.key.text)
             
             /*def dirItem = siteItemService.getSiteItem(dirName)*/
             recTenant.dirItem = dirItem
-            recTenant.title = dirItem.queryValue('name_s')
+            /*recTenant.title = dirItem.queryValue('name_s')*/
             recTenant.key = dir.key
-            recTenant.url = dir.storeUrl
+            /*recTenant.url = dir.storeUrl*/
             recTenants << recTenant
             
                         /*
