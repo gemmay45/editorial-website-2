1972a5dfd5badffc15a5ba8bd213efa67aad802b
admin admin <evaladmin@example.com>
2023-11-29 21:06:18 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -17,7 +17,7 @@
             recTenant.dirItem = dirItem
             recTenant.title = dirItem.queryValue('name_s')
             recTenant.tagline = dirItem.queryValue('tagline_s')
-            recTenant.key = dir.key
+
             recTenants << recTenant
             
                         /*
