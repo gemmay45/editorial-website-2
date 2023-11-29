58646abe56e21a9787ad10db2d02b7c1294da048
admin admin <evaladmin@example.com>
2023-11-29 11:57:22 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,6 +1,9 @@
-def category = contentModel.recommended_o.item
-def maxTenants = contentModel.maxTenants_i
-def searchHelper = new TenantSearchHelper(searchClient, urlTransformationService)
-def tenants = searchHelper.searchTenants(false, category, segment, 0, maxTenants)
 
-templateModel.tenants = tenants
+    def recTenants = []
+def recTenant = [:]
+recTenant.title = "aaa"
+recTenants << article
+recTenant.title = "bbb"
+recTenants << article
+
+templateModel.recTenant = recTenants
