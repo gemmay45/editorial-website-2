138070bd65b33a769bb5760591c10d5b5493f972
admin admin <evaladmin@example.com>
2023-11-29 10:57:16 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,1 +1,11 @@
-
+templateModel.getNavIcon = { item ->
+  def storeUrl = urlTransformationService.transform("renderUrlToStoreUrl", item.url)
+  def siteItem = siteItemService.getSiteItem(storeUrl)
+  if(siteItem) {
+    def navIcon = siteItem.navIcon?.text
+    if(navIcon) {
+      return navIcon
+    }
+  }
+  return "fa-file-o"
+}
