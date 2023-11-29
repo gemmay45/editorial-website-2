45b90ae28bbffd92e65669ea112695a33f923855
admin admin <evaladmin@example.com>
2023-11-29 16:59:53 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,34 +1,26 @@
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-    def recTenants = []
-    def topNavItems = [:]
-    def dirs = contentModel.recommended_o.item
-    dirs.each { dir ->
-            def recTenant = [:]
-            
-            def dirName = dir.key.text
-            def dirItem = siteItemService.getSiteItem(dir.key.text)
-            
-            recTenant.id = dirItem.objectId
-            recTenant.objectId = dirItem.objectId
-            recTenant.path = dirItem.localId
-            recTenant.storeUrl = dirItem.storeUrl
-            recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", recTenant.storeUrl)
+import org.craftercms.sites.editorial.TenantSearchHelper
+import org.craftercms.sites.editorial.ProfileUtils
 
-            recTenant.dirItem = dirItem
-            recTenant.title = dirItem.queryValue('name_s')
-            recTenant.key = dir.key
-            recTenants << recTenant
-            
-                        /*
-                        def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
-            
+def segment = ProfileUtils.getSegment(profile, siteItemService)
+def category = contentModel.category_s
+def maxTenants = contentModel.maxTenants_i
+def searchHelper = new TenantSearchHelper(searchClient, urlTransformationService)
+def tenants = searchHelper.searchTenants(false, category, segment, 0, maxTenants)
 
-            if (dirItem != null) {
-                def dirDisplayName = dirItem.queryValue('internal-name')
-                   topNavItems.put(dirName, dirDisplayName)
-            }
-            */
-   }
-
-
-templateModel.recTenants = recTenants
+templateModel.tenants = tenants
