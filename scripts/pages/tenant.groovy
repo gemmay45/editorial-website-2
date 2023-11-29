e5b3efbde1ae916303435ec64902e05723704666
admin admin <evaladmin@example.com>
2023-11-29 13:08:17 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -5,7 +5,7 @@
     def dirs = contentModel.recommended_o.item
     dirs.each { dir ->
             def dirName = dir.key
-            def dirItem = siteItemService.getSiteItem("${dirName}")
+            def dirItem = siteItemService.getSiteItem(${dirName})
             recTenant.title = "aaa"
             recTenant.key = dirItem.storeUrl
             recTenants << recTenant
