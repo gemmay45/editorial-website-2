2351e8ccc08be8c71b09441ade1141098c6cea2e
admin admin <evaladmin@example.com>
2023-11-29 11:13:11 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -11,30 +11,3 @@ templateModel.getNavIcon = { item ->
   return "fa-file-o"
 }
 
-                        <#assign tenantItem =  siteItemService.getSiteItem(tenant.key) />
-                        <@crafter.div $model=tenantItem>
-                            <#assign url = tenantItem.storeUrl
-                                ?replace("/site/website", "")
-                                ?replace("index.xml", "")
-                            />
-        
-        				    <#assign name_field = "name_s" />
-    				        <#assign tenantName = tenantItem[name_field] />
-    				        <#assign tagline_field = "tagline_s" />
-    				        <#assign tagline = tenantItem[tagline_field] />
-    
-                            <@crafter.a $model=tenantItem href="${url}">
-                                <@crafter.img
-                                  $model=tenantItem
-                                  $field="images_o"
-                                  src="${tenantItem.images_o.item[0].image_s}"
-                                  alt=""
-                                  width="112px"
-                                />
-                                
-                                <@crafter.span $model=tenantItem>${tenantName}</@crafter.span>
-                                <br/>
-                                <@crafter.span $model=tenantItem class="location-card__tagline">${tagline!""}</@crafter.span>
-                                <br/><br/>
-                            </@crafter.a>
-                        </@crafter.div>
