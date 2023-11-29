db58bfa1ca952ce5e554b2b67b18bd78c65c9fb9
admin admin <evaladmin@example.com>
2023-11-29 15:07:23 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -13,7 +13,7 @@
             recTenant.path = dirItem.localId
             recTenant.storeUrl = dirItem.localId
             /*recTenant.title = dirItem.queryValue('name_s')*/
-            recTenant.url = urlTransformationService.transform("storeUrlToRenderUrl", dir.localId)
+            recTenant.url = dir.localId /*urlTransformationService.transform("storeUrlToRenderUrl", dir.localId)*/
 
             
             /*def dirItem = siteItemService.getSiteItem(dirName)*/
