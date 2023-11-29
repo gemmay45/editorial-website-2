f680d97c01d7baa2e7a12599cc1f4dfdb3b1a8cc
admin admin <evaladmin@example.com>
2023-11-29 11:06:53 +0800

    "User admin wrote content /scripts/pages/tenant.groovy"

--- scripts/pages/tenant.groovy
+++ scripts/pages/tenant.groovy
@@ -1,6 +1,7 @@
 templateModel.getNavIcon = { item ->
   def storeUrl = urlTransformationService.transform("renderUrlToStoreUrl", item.url)
   def siteItem = siteItemService.getSiteItem(storeUrl)
+  return siteItem
   if(siteItem) {
     def navIcon = siteItem.navIcon?.text
     if(navIcon) {
