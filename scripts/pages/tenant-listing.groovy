7665ceb36966573d0972efc3b2cb67f6f51dec11
admin admin <evaladmin@example.com>
2023-11-29 22:28:09 +0800

    "User admin wrote content /scripts/pages/tenant-listing.groovy"

--- scripts/pages/tenant-listing.groovy
+++ scripts/pages/tenant-listing.groovy
@@ -18,7 +18,7 @@ import org.craftercms.sites.editorial.TenantSearchHelper
 import org.craftercms.sites.editorial.ProfileUtils
 
 def segment = ProfileUtils.getSegment(profile, siteItemService)
-def category = contentModel.category_s
+def category = contentModel.cateories_o
 def maxTenants = contentModel.maxTenants_i
 def searchHelper = new TenantSearchHelper(searchClient, urlTransformationService)
 def tenants = searchHelper.searchTenants(false, category, segment, 0, maxTenants)
