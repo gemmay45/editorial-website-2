4f261a89fe34fe40363232c3a6b4490d65eda4cd
admin admin <evaladmin@example.com>
2023-11-30 09:50:58 +0800

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/TenantSearchHelper.groovy"

--- scripts/classes/org/craftercms/sites/editorial/TenantSearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/TenantSearchHelper.groovy
@@ -238,7 +238,7 @@ class TenantSearchHelper {
         def categories = []
         def categoryItems = doc.categories_o.item
         categoryItems.each { item ->
-            def category = item.field('value_smv')
+            def category = item
 
             categories << category
         }
